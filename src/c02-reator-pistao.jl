be80c2ebb3293664adde6c8a857dedd30ed313d6
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2023-09-25 21:39:39 +0200

    Some progress

--- src/c02-reator-pistao.jl
+++ src/c02-reator-pistao.jl
@@ -12,6 +12,7 @@ begin
 
     using CairoMakie
     using DocStringExtensions
+    using Polynomials
     using Printf
     using Roots
     using SparseArrays: spdiagm
@@ -33,9 +34,7 @@ md"""
 
 # ╔═╡ 7912192d-1528-48ce-9adc-7e6a26b25c51
 md"""
-## Reator com fase sólida e gas
-
-**TODO**
+## Reator com fase sólida e gás
 """
 
 # ╔═╡ 975744de-7ab0-4bfa-abe5-3741ec7ec1cf
@@ -43,9 +42,9 @@ md"""
 ## Anexos
 """
 
-# ╔═╡ be3a4933-516e-4867-a801-4df4f695432a
+# ╔═╡ 27233ad1-a588-48d6-9103-901a71936074
 md"""
-### Funções
+### Estruturas de dados
 """
 
 # ╔═╡ c9194590-0fc7-4d68-9b19-9ee8d415fbda
@@ -72,8 +71,11 @@ struct PFRData
     "Matriz do problema."
     K::SparseMatrixCSC{Float64, Int64}
 
-    "Temperatura do fluido na última iteração [K]"
+    "Temperatura do fluido na última iteração [K]."
     U::Vector{Float64}
+
+    "Fluxo mássico através do reator [kg/s]."
+    ṁ::Float64
 
     """	Construtor interno dos dados de reatores.
 
@@ -99,18 +101,26 @@ struct PFRData
             ρ::Float64
         )
         δ = L / N
-
         z = collect(0.0:δ:L)
         T = T₀ * ones(N+1)
         K = 2spdiagm(-1=>-ones(N-1), 0=>ones(N))
-        a = (ĥ * P * δ) / (ρ * u * A)
+        ṁ = ρ * u * A
+        a = (ĥ * P * δ) / ṁ
         b = ones(N)
-
         T[1] = T₀
-
-        return new(z, T, h, a, b, K, copy(T))
+        return new(z, T, h, a, b, K, copy(T), ṁ)
     end
 end
+
+# ╔═╡ 5007aa03-6db0-4626-a73c-9172fef2c4ea
+md"""
+### Criação de exemplos
+"""
+
+# ╔═╡ be3a4933-516e-4867-a801-4df4f695432a
+md"""
+### Funções físicas
+"""
 
 # ╔═╡ 5a492522-9db4-44bc-80d4-6aca529560de
 """
@@ -129,7 +139,7 @@ function solveenthalpypfr(;
     α::Float64 = 0.4,
     ε::Float64 = 1.0e-08,
 )::Bool
-    residual = -ones(M)
+    residual = -1.0
     niter = 0
 
     T = @view r.T[1:end]
@@ -149,9 +159,11 @@ function solveenthalpypfr(;
 
         T[2:end] = U + α * Δ
 
-        residual[niter] = maximum(abs.(Δ))
+        residual = maximum(abs.(Δ))
 
-        if (residual[niter] <= ε)
+        if (residual <= ε)
+            # @info("Laço interno convergiu após $(niter) iterações")
+
             Δ = maximum(abs.(T - r.U))
 
             r.U[1:end] = T[1:end]
@@ -160,8 +172,35 @@ function solveenthalpypfr(;
         end
     end
 
-    error("Simulação falhou em convergir")
+    @warn("Laço interno: após $(niter) iterações → $(residual)")
+    return false
 end
+
+# ╔═╡ 57c91e12-cc67-41ea-8554-8135123940bd
+"Interpola temperaturas de superfície nas interfaces."
+function getsurfacetemperature(r)
+    T = reverse(r.T)
+    return (T[1:end-1] + T[2:end]) / 2
+end
+
+# ╔═╡ 75843461-2fef-43b6-9560-cdf7a587534e
+"Variação de entalpia entre entrada e saída do reator."
+function enthalpychange(r::PFRData)::Float64
+    return r.h(r.T[end]) - r.h(r.T[1])
+end
+
+# ╔═╡ 916238a8-093d-4dec-b95a-edcbf8b766e3
+"Resíduo relativo da conservação da entalpia."
+function enthalpyresidual(ra, rb)
+    Δha = ra.ṁ * enthalpychange(ra)
+    Δhb = rb.ṁ * enthalpychange(rb)
+    return abs(Δhb + Δha) / abs(Δhb)
+end
+
+# ╔═╡ 1e48a850-f92c-4a7e-aef8-8e52e45eba30
+md"""
+### Funções gráficas
+"""
 
 # ╔═╡ 05457a3d-ec99-4e06-a969-a6e3804f62d4
 "Base para traçar temperatura ao longo do reator."
@@ -177,17 +216,14 @@ function createpfrplot(; L)
     return fig, ax
 end
 
-# ╔═╡ 57c91e12-cc67-41ea-8554-8135123940bd
-"Interpola temperaturas de superfície nas interfaces."
-function getsurfacetemperature(r)
-    T = reverse(r.T)
-    return (T[1:end-1] + T[2:end]) / 2
-end
-
 # ╔═╡ 0e2360a8-0653-4faa-b909-1ab84ca9fbac
 md"""
 ### Dados
 """
+
+# ╔═╡ f307de6d-f197-47d6-a1a6-b1262e7569e0
+"Constante dos gases ideais [J/(mol.K)]"
+const GAS_CONSTANT = 8.314_462_618_153_24
 
 # ╔═╡ d76d9e69-8830-4190-994f-49689d44b506
 "Comprimento do reator [m]"
@@ -195,17 +231,72 @@ const L::Float64 = 10.0
 
 # ╔═╡ d24c4ccc-70a3-4e37-a800-e57d54df3b48
 "Ilustração padronizada para a simulação exemplo."
-function plotpfrpair(r₁, r₂)
+function plotpfrpair(r₁, r₂; ylim = (300, 400), loc = :rb)
     z₁ = r₁.z
     z₂ = L .- z₁
 
     fig, ax = createpfrplot(; L = L)
     stairs!(ax, z₁, r₁.T, label = "r₁", color = :blue, step = :center)
     stairs!(ax, z₂, r₂.T, label = "r₂", color = :red, step = :center)
-    ax.yticks = range(300, 400, 6)
-    ylims!(ax, (300, 400))
-    axislegend(position = :rb)
+    ax.yticks = range(ylim..., 6)
+    ylims!(ax, ylim)
+    axislegend(position = loc)
     fig
+end
+
+# ╔═╡ d684dc5e-d78d-484f-a820-317406d9a41a
+"Plot results of standard PFR solution"
+function plotgassolidpfr(; gas, sol = nothing)
+    xlaba = "Position [m]"
+    ylab1 = "Temperature [K]"
+    ylab2 = "Velocity [m/s]"
+    ylab3 = "Density [kg/m³]"
+    ylab4 = "Pressure [Pa]"
+
+    xlims = (0.0, L)
+    xticks = range(xlims..., 6)
+
+    fig = Figure(resolution = (1000, 700))
+    axes = fig[1, 1] = GridLayout()
+
+    ax1 = Axis(axes[1, 1], ylabel = ylab1, xlabel = xlaba, xticks = xticks)
+    ax2 = Axis(axes[2, 1], ylabel = ylab2, xlabel = xlaba, xticks = xticks)
+    ax3 = Axis(axes[1, 2], ylabel = ylab3, xlabel = xlaba, xticks = xticks)
+    ax4 = Axis(axes[2, 2], ylabel = ylab4, xlabel = xlaba, xticks = xticks)
+
+    xlims!(ax1, xlims)
+    xlims!(ax2, xlims)
+    xlims!(ax3, xlims)
+    xlims!(ax4, xlims)
+
+    linkxaxes!(ax1, ax2, ax3, ax4)
+
+    z = gas[:z]
+    T = gas[:T]
+    u = gas[:u]
+    Ρ = gas[:Ρ]
+    p = gas[:p] .- 101_325.0
+
+    lines!(ax1, z, T, label = "Gas")
+    lines!(ax2, z, u, label = "Gas")
+    lines!(ax3, z, Ρ, label = "Gas")
+    lines!(ax4, z, p, label = "Gas")
+
+    if !isnothing(sol)
+        z = reverse(sol[:z])
+        T = sol[:T]
+        u = sol[:u]
+
+        lines!(ax1, z, T, label = "Solid")
+        lines!(ax2, z, u, label = "Solid")
+    end
+
+    axislegend(ax1)
+    axislegend(ax2)
+    axislegend(ax3)
+    axislegend(ax4)
+
+    return fig
 end
 
 # ╔═╡ 243ce122-61cf-4697-9f13-1710afef8e5c
@@ -226,11 +317,15 @@ const u::Float64 = 1.0
 
 # ╔═╡ 0e2aa152-976d-400b-ae25-71f37c90e01c
 "Mass específica do fluido [kg/m³]"
-const ρ::Float64 = 1000.0
+const ρˡ::Float64 = 1000.0
+
+# ╔═╡ e125fd83-5fd1-4c5c-b466-22fd3375ff4a
+"Mass específica do sólido [kg/m³]"
+const ρˢ::Float64 = 3000.0
 
 # ╔═╡ 901d3be7-9a5c-4638-98ca-b4bb0ce355ed
 "Calor específico do fluido [J/(kg.K)]"
-const cₚ::Float64 = 4182.0
+const cₚˡ::Float64 = 4182.0
 
 # ╔═╡ a65e3986-9683-463b-b4d7-c78f80750328
 "Cria um par padronizado de reatores para simulação exemplo.
@@ -248,10 +343,10 @@ function createprfpair(N)
     ĥ = 5000.0
 
     shared = (L = L, N = N, P = P, A = A,
-              ĥ = ĥ, u = u, ρ = ρ)
+              ĥ = ĥ, u = u, ρ = ρˡ)
 
-    r₁ = PFRData(; h = (T) -> 1cₚ * T, T₀ = T₁, shared...)
-    r₂ = PFRData(; h = (T) -> 3cₚ * T, T₀ = T₂, shared...)
+    r₁ = PFRData(; h = (T) -> 1cₚˡ * T, T₀ = T₁, shared...)
+    r₂ = PFRData(; h = (T) -> 3cₚˡ * T, T₀ = T₂, shared...)
 
     return r₁, r₂
 end
@@ -274,7 +369,75 @@ let
         end
     end
 
+    println("Resíduo = $(enthalpyresidual(r₁, r₂))")
     plotpfrpair(r₁, r₂)
+end
+
+# ╔═╡ 595edaed-4a16-43f4-b3b2-4803f06f93e7
+"Calor específico do gás [J/(kg.K)]"
+const cₚᵍ = Polynomial([
+    959.8458126240355,
+    0.3029051601580761,
+    3.988896105280984e-05,
+    -6.093647929461819e-08,
+    1.0991100692950414e-11
+], :T)
+
+# ╔═╡ b30e0fb9-d505-41e5-8a7d-1e8f2cbc2bbc
+"Variação de entalpia do gás [J/(kg.K)]"
+const hᵍ = integrate(cₚᵍ)
+
+# ╔═╡ 630ba246-b5fb-4f9d-a9d2-b2edb471ada0
+"Cria um par padronizado de reatores para simulação exemplo."
+function createprfgassolidpair(N)
+    P = 2R
+    A = 0.5π * R^2
+    ĥ = 10.0
+
+    shared = (L = L, N = N, P = P, A = A, ĥ = ĥ)
+
+    ratio = 2/3
+
+    Tᵍ = 2000.0
+    ρᵍ = (101325.0 * 0.02897530345830224) / (GAS_CONSTANT * Tᵍ)
+
+    uᵍ = 3u
+    ṁᵍ = ρᵍ * uᵍ * A
+
+    ṁˢ = ratio * ṁᵍ
+    uˢ = ṁˢ / (ρˢ * A)
+
+    r₁ = PFRData(; h = (T) -> cₚˡ * T, T₀ = T₁, shared..., u = uˢ, ρ = ρˢ)
+    r₂ = PFRData(; h = (T) -> hᵍ(T),   T₀ = Tᵍ, shared..., u = uᵍ, ρ = ρᵍ)
+
+    return r₁, r₂
+end
+
+# ╔═╡ c69be00a-40d4-4c25-aa47-ffb38ccaecec
+let
+    N = 200
+    r₁, r₂ = createprfgassolidpair(N)
+
+    @time for k in 1:100
+        α = (k < 10) ? 0.3 : 0.9
+        ε = 0.01
+
+        Tₛ = getsurfacetemperature(r₂)
+        c₁ = solveenthalpypfr(; r = r₁, Tₛ = Tₛ, α = α, ε = ε)
+
+        Tₛ = getsurfacetemperature(r₁)
+        c₂ = solveenthalpypfr(; r = r₂, Tₛ = Tₛ, α = α, ε = ε)
+
+        δh = enthalpyresidual(r₁, r₂)
+
+        if c₁ && c₂ && δh < 1.0e-03
+            @info("Laço externo convergiu após $(k) iterações")
+            break
+        end
+    end
+
+    println("Conservação da entalpia = $(enthalpyresidual(r₁, r₂))")
+    plotpfrpair(r₁, r₂; ylim = (300, 2000), loc = :lt)
 end
 
 # ╔═╡ 54aa6060-a605-4a05-83f1-2b672f1d148f
@@ -287,21 +450,33 @@ md"""
 # ╟─f3b7d46f-0fcc-4f68-9822-f83e977b87ee
 # ╟─e5d12839-8167-4ddf-843f-f8ad0f682126
 # ╟─7912192d-1528-48ce-9adc-7e6a26b25c51
+# ╟─c69be00a-40d4-4c25-aa47-ffb38ccaecec
 # ╟─975744de-7ab0-4bfa-abe5-3741ec7ec1cf
-# ╟─be3a4933-516e-4867-a801-4df4f695432a
+# ╟─27233ad1-a588-48d6-9103-901a71936074
 # ╟─c9194590-0fc7-4d68-9b19-9ee8d415fbda
-# ╟─5a492522-9db4-44bc-80d4-6aca529560de
-# ╟─05457a3d-ec99-4e06-a969-a6e3804f62d4
+# ╟─5007aa03-6db0-4626-a73c-9172fef2c4ea
 # ╟─a65e3986-9683-463b-b4d7-c78f80750328
-# ╟─d24c4ccc-70a3-4e37-a800-e57d54df3b48
+# ╟─630ba246-b5fb-4f9d-a9d2-b2edb471ada0
+# ╟─be3a4933-516e-4867-a801-4df4f695432a
+# ╟─5a492522-9db4-44bc-80d4-6aca529560de
 # ╟─57c91e12-cc67-41ea-8554-8135123940bd
+# ╟─916238a8-093d-4dec-b95a-edcbf8b766e3
+# ╟─75843461-2fef-43b6-9560-cdf7a587534e
+# ╟─1e48a850-f92c-4a7e-aef8-8e52e45eba30
+# ╟─05457a3d-ec99-4e06-a969-a6e3804f62d4
+# ╟─d24c4ccc-70a3-4e37-a800-e57d54df3b48
+# ╟─d684dc5e-d78d-484f-a820-317406d9a41a
 # ╟─0e2360a8-0653-4faa-b909-1ab84ca9fbac
+# ╟─f307de6d-f197-47d6-a1a6-b1262e7569e0
 # ╟─d76d9e69-8830-4190-994f-49689d44b506
 # ╟─243ce122-61cf-4697-9f13-1710afef8e5c
 # ╟─25f64410-97d0-43f7-a8b1-de0983265a7c
 # ╟─012242f9-962d-46fe-9619-46906d2d8478
 # ╟─fe7b4804-e952-41ff-b2fd-20972035c747
 # ╟─0e2aa152-976d-400b-ae25-71f37c90e01c
+# ╟─e125fd83-5fd1-4c5c-b466-22fd3375ff4a
 # ╟─901d3be7-9a5c-4638-98ca-b4bb0ce355ed
+# ╟─595edaed-4a16-43f4-b3b2-4803f06f93e7
+# ╟─b30e0fb9-d505-41e5-8a7d-1e8f2cbc2bbc
 # ╟─54aa6060-a605-4a05-83f1-2b672f1d148f
 # ╟─fe2c3680-5b91-11ee-282c-c74d3b01ef9b
