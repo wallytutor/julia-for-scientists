c1073dc1d046a5f1986e8a2fa14d110cf78d0982
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2023-10-18 19:49:05 +0200

    Added draft

--- src/b03-transporte-difusao.jl
+++ src/b03-transporte-difusao.jl
@@ -428,6 +428,99 @@ md"""
 ### Pacotes
 """
 
+# ╔═╡ dd839c47-8340-4cdf-bb32-067ba293d522
+md"""
+```math
+\frac{\partial{}x}{\partial{}t}=\nabla\cdotp{}(D(x)\nabla{}T)
+```
+
+```math
+\frac{\partial{}x}{\partial{}t}=
+\frac{\partial}{\partial{}x}
+\left(D(x)\frac{\partial{}x}{\partial{}z}\right)
+```
+
+```math
+\int_{s}^{n}\int_{0}^{\tau}
+\frac{\partial{}x}{\partial{}t}dtdz=
+\int_{0}^{\tau}\int_{s}^{n}
+\frac{\partial}{\partial{}z}
+\left(D(x)\frac{\partial{}x}{\partial{}z}\right)dzdt
+```
+
+```math
+\left(x_P^{\tau}-x_P^{0}\right)(w_{n}-w_{s})=
+\int_{0}^{\tau}
+\left(D(x)\frac{\partial{}T}{\partial{}r}\right)\bigg\vert_{s}^{n}dt
+```
+
+"""
+
+# ╔═╡ f4e14699-a034-40d0-9219-4b5d4efada6b
+md"""
+```math
+\begin{align}
+\left(x_P^{\tau}-x_P^{0}\right)\frac{(w_{n}-w_{s})}{\tau}&=
+f\left[
+D(x_n)\frac{x_N^{\tau}-x_P^{\tau}}{\delta_{P,N}}-
+D(x_s)\frac{x_P^{\tau}-x_S^{\tau}}{\delta_{P,S}}
+\right]\\[8pt]
+&+(1-f)\left[
+D(x_n)\frac{x_N^{0}-x_P^{0}}{\delta_{P,N}}-
+D(x_s)\frac{x_P^{0}-x_S^{0}}{\delta_{P,S}}
+\right]
+\end{align}
+```
+
+```math
+\begin{align}
+\alpha_{P}  & = \frac{(w_{n}-w_{s})}{\tau}\\[8pt]
+\beta_{j}   & = \frac{D(x_j)}{\delta_{P,J}}
+\end{align}
+```
+
+```math
+-f\beta_{s}x_S+
+(\alpha_{P}+f\beta_{n}+f\beta_{s})x_P
+-f\beta_{n}x_N
+=
+g\beta_{s}x_S^{0}+
+(\alpha_{P}-g\beta_{n}-g\beta_{s})x_P^{0}+
+g\beta_{n}x_N^{0}
+```
+"""
+
+# ╔═╡ fe2492ee-d95c-4582-9ccd-a27763705393
+md"""
+```math
+-\beta_{s}x_S+
+(\alpha_{P}+\beta_{n}+\beta_{s})x_P
+-\beta_{n}x_N
+=
+\alpha_{P}x_P^{0}
+```
+
+```math
+\begin{align}
+a_{S} & = -\beta_{s}\\[8pt]
+a_{N} & = -\beta_{n}\\[8pt]
+a_{P} & = \alpha_{P}+\beta_{n}+\beta_{s}
+\end{align}
+```
+
+```math
+a_Sx_S + a_Px_P + a_Nx_N = \alpha_{P}x_P^{0}
+```
+
+```math
+a_1x_P + a_Nx_N = \alpha_{P}x_P^{0}\quad\text{where}\quad{}a_1=\alpha_{P}+\beta_{n}
+```
+
+```math
+a_Sx_S + a_Rx_P = \alpha_{P}x_P^{0}+hx_\infty\quad\text{where}\quad{}a_R=\alpha_{P}+h+\beta_{s}
+```
+"""
+
 # ╔═╡ Cell order:
 # ╟─30c163e1-4af6-4776-887b-2835cf488407
 # ╟─be9ac055-bccf-41d1-9616-8893d51b980d
@@ -453,8 +546,11 @@ md"""
 # ╟─8b9cf875-78b6-466a-9e43-f3eceb8d027a
 # ╟─f9a24b0f-d820-48d1-a048-853dd994a249
 # ╟─63a9fa87-92f1-444a-8bcf-dde5d682e099
-# ╟─8fe591cd-f3f7-4cf8-8d91-92c6dff1dd01
+# ╠═8fe591cd-f3f7-4cf8-8d91-92c6dff1dd01
 # ╠═efed88fb-9b89-4896-acc3-cec5f3462978
 # ╟─7b591c27-18d8-4022-9b6a-bb0c0ccce888
-# ╟─73b13fb0-62b6-11ee-27bf-69788069829a
+# ╠═73b13fb0-62b6-11ee-27bf-69788069829a
 # ╟─f6cdb233-9b99-48ff-bce4-4f359a35f591
+# ╟─dd839c47-8340-4cdf-bb32-067ba293d522
+# ╟─f4e14699-a034-40d0-9219-4b5d4efada6b
+# ╟─fe2492ee-d95c-4582-9ccd-a27763705393
